7e44781ab941d7aaecf28c5f5848e6cad0089eb5
Piemoroni <moronipietra@gmail.com>
2025-03-11 14:34:38 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	/*
+	Inteiro (Integer) int %d
+	Real (Float) float %f
+	Cracter (Character) char %c
+	Boleano (Boolean) bool %d 
+	*/
+	
+	int num = 10;
+	
+	num = 10;
+	
+	printf("A variavel num vale : %d\r\n", num);
+	
+	printf("Ola, Mundo !");
+	return 0;
+}
